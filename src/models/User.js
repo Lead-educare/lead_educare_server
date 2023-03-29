814de3f4565ad224c8d6f7af835e3a0a37d5a311
Parvej Hossain <muhammadparvej98@gmail.com>
2023-03-30 01:11:18 +0600

    setup

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,16 @@
-const {Schema, model} = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
+const { Schema, model } = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-
-
-
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     email: {
-        type: String,
-        validate: [validator.isEmail, "Provide a valid Email"],
-        trim: true,
-        lowercase: true,
-        required: [true, "Email address is required"],
-        unique: true
+      type: String,
+      validate: [validator.isEmail, "Provide a valid Email"],
+      trim: true,
+      lowercase: true,
+      required: [true, "Email address is required"],
+      unique: true,
     },
     mobile: {
       type: String,
@@ -20,88 +18,87 @@ const userSchema = new Schema({
       maxLength: 11,
       validate: [validator.isMobilePhone, 'Provide a valid mobile number'],
       unique: true
+
     },
     firstName: {
-        type: String,
-        required: [true, 'First name is required'],
-        minLength: [3, 'First name must be 3 character'],
-        maxLength: [100, 'First name is too large'],
-        trim: true,
-        lowercase: true
+      type: String,
+      required: [true, "First name is required"],
+      minLength: [3, "First name must be 3 character"],
+      maxLength: [100, "First name is too large"],
+      trim: true,
+      lowercase: true,
     },
     lastName: {
-        type: String,
-        required: [true, 'Last name is required'],
-        minLength: [3, 'Last name must be 3 character'],
-        maxLength: [100, 'Last name is too large'],
-        trim: true,
-        lowercase: true,
-
+      type: String,
+      required: [true, "Last name is required"],
+      minLength: [3, "Last name must be 3 character"],
+      maxLength: [100, "Last name is too large"],
+      trim: true,
+      lowercase: true,
     },
     password: {
-        type: String,
-        required: [true, 'Password is required'],
-        validate:{
-            validator: (value)=>
-                validator.isStrongPassword(value, {
-                    minLength: 8,
-                    minUppercase: 1,
-                    minNumbers: 1,
-                    minSymbols: 1,
-                    minLowercase: 1
-                }),
-            message: 'Please provide a strong password'
-        }
+      type: String,
+      // required: [true, "Password is required"],
+      // validate: {
+      //   validator: (value) =>
+      //     validator.isStrongPassword(value, {
+      //       minLength: 8,
+      //       minUppercase: 1,
+      //       minNumbers: 1,
+      //       minSymbols: 1,
+      //       minLowercase: 1,
+      //     }),
+      //   message: "Please provide a strong password",
+      // },
+    },
+    picture: {
+      type: String,
     },
     confirmPassword: {
-        type: String,
-        required: [true, 'Confirm Password is required'],
-        validate: {
-            validator: function(value){
-                return value === this.password
-            },
-            message: 'Password does not match'
-        }
+      type: String,
+      // required: [true, "Confirm Password is required"],
+      // validate: {
+      //   validator: function (value) {
+      //     return value === this.password;
+      //   },
+      //   message: "Password does not match",
+      // },
     },
 
-    roleId: { type: Schema.Types.ObjectId, ref: 'Role' },
+    roleId: { type: Schema.Types.ObjectId, ref: "Role" },
     status: {
-        type: String,
-        enum: ['active', 'inactive', 'blocked'],
-        default: 'active'
+      type: String,
+      enum: ["active", "inactive", "blocked"],
+      default: "active",
     },
 
     verified: {
-        type: Boolean,
-        default: false
-    }
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-}, {versionKey: false, timestamps: true});
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.password === "") {
+    return next();
+  }
 
-
-
-userSchema.pre('save', function(next){
-    if(!this.isModified('password')){
-        return next();
-    }
-
-    const password = this.password;
-    this.password = bcrypt.hashSync(password);
-    this.confirmPassword = undefined;
-    next();
+  const password = this.password;
+  this.password = bcrypt.hashSync(password);
+  this.confirmPassword = undefined;
+  next();
 });
 
+userSchema.methods.comparePassword = function (password, hash) {
+  return bcrypt.compareSync(password, hash);
+};
 
-userSchema.methods.comparePassword = function(password, hash){
-    return bcrypt.compareSync(password, hash);
-}
+userSchema.methods.hashPassword = function (password) {
+  return bcrypt.hashSync(password);
+};
 
-userSchema.methods.hashPassword = function(password){
-    return bcrypt.hashSync(password);
-}
-
-
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
-
