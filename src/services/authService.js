370dcf46d7d8b291b6819ad6751fd7ccdb3aebf3
Parvej Hossain <muhammadparvej98@gmail.com>
2023-04-02 00:05:48 +0600

    social login feature added

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,146 +1,239 @@
 const userService = require("./userService");
-const error = require('../helpers/error');
+const error = require("../helpers/error");
 const otpService = require("./otpService");
-const sendOTP = require('../helpers/sendOTP');
-const authHelper = require('../helpers/authHelper');
-const rolePermissionService = require('../services/admin/rolePermissionService');
+const sendOTP = require("../helpers/sendOTP");
+const authHelper = require("../helpers/authHelper");
+const rolePermissionService = require("../services/admin/rolePermissionService");
 
-const registerService = async (
-    {email, mobile, firstName, lastName, password, confirmPassword, role = 'user'}
-) => {
-    const isMatch = await userService.findUserByProperty('email', email);
-    if (isMatch) throw error('Email already taken', 400);
+const registerService = async ({
+  email,
+  mobile,
+  firstName,
+  lastName,
+  password,
+  confirmPassword,
+  role = "user",
+}) => {
+  const isMatch = await userService.findUserByProperty("email", email);
+  if (isMatch) throw error("Email already taken", 400);
 
-    const isOtp = await otpService.findOptByProperty({email: email});
-    const otp = Math.floor(100000 + Math.random() * 900000);
+  const isOtp = await otpService.findOptByProperty({ email: email });
+  const otp = Math.floor(100000 + Math.random() * 900000);
 
-    if (isOtp) {
-        await otpService.updateOtp({email, otp, status: 0})
-    } else {
-        await otpService.createOtp({email, otp});
+  if (isOtp) {
+    await otpService.updateOtp({ email, otp, status: 0 });
+  } else {
+    await otpService.createOtp({ email, otp });
+  }
+  // Email Send
+  const send = await sendOTP(
+    email,
+    "Your Verification Code is= " + otp,
+    `${process.env.APP_NAME} email verification`
+  );
+
+  if (send[0].statusCode === 202) {
+    let isRole = await rolePermissionService.roleFindByProperty("name", "user");
+    if (!isRole) {
+      isRole = await rolePermissionService.createNewRoleService({
+        roleName: role,
+      });
     }
-    // Email Send
-    const send = await sendOTP(email, "Your Verification Code is= " + otp, `${process.env.APP_NAME} email verification`)
-
-    if (send[0].statusCode === 202) {
-
-        let isRole = await rolePermissionService.roleFindByProperty('name', 'user');
-        if (!isRole){
-            isRole = await rolePermissionService.createNewRoleService({roleName: role});
-        }
-        return await userService.createNewUser({email, mobile, firstName, lastName, password, confirmPassword, roleId: isRole?._id});
-    } else {
-        throw error('Server error occurred', 5000)
-    }
-
+    return await userService.createNewUser({
+      email,
+      mobile,
+      firstName,
+      lastName,
+      password,
+      confirmPassword,
+      roleId: isRole?._id,
+    });
+  } else {
+    throw error("Server error occurred", 5000);
+  }
 };
 
-const loginService = async (
-    {email, password}
-) => {
+const loginService = async ({ email, password }) => {
+  const user = await userService.findUserByProperty("email", email);
+  if (!user) throw error("Email or password do not match", 400);
 
-    const user = await userService.findUserByProperty('email', email);
-    if (!user) throw error('Email or password do not match', 400);
+  const isMatch = authHelper.comparePassword({ password, hash: user.password });
+  if (!isMatch) throw error("Email or password do not match", 400);
 
-    const isMatch = authHelper.comparePassword({password, hash: user.password});
-    if (!isMatch) throw error('Email or password do not match', 400);
+  if (!user?.verified)
+    throw error("Your account is not verify. please verify your account", 400);
+  if (user?.status !== "active")
+    throw error(
+      "Your account is not active. please contact Administrator",
+      400
+    );
 
-    if (!user?.verified) throw error('Your account is not verify. please verify your account', 400);
-    if (user?.status !== 'active') throw error('Your account is not active. please contact Administrator', 400);
+  const payload = {
+    _id: user._id,
+    email: user.email,
+    mobile: user.mobile,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    status: user.status,
+    verified: user.verified,
+    role: {
+      _id: user.role._id,
+      name: user.role.name,
+    },
+    permissions: user.permissions,
+  };
+  return authHelper.createToken(payload);
+};
+
+const socialLoginService = async (userData) => {
+  const { email, firstName, lastName, mobile, picture } = userData;
+  const user = await userService.findUserByProperty("email", email);
+  if (!user) {
+    let isRole = await rolePermissionService.roleFindByProperty("name", "user");
+
+    if (!isRole) {
+      isRole = await rolePermissionService.createNewRoleService({
+        roleName: "user",
+      });
+    }
+
+    await userService.createNewUser({
+      email,
+      mobile,
+      firstName,
+      lastName,
+      password: "",
+      confirmPassword: "",
+      picture,
+      verified: true,
+      roleId: isRole?._id,
+    });
+
+    let user = await userService.findUserByProperty("email", email);
 
     const payload = {
-        _id: user._id,
-        email: user.email,
-        mobile: user.mobile,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        status: user.status,
-        verified: user.verified,
-        role: {
-            _id: user.role._id,
-            name: user.role.name
-        },
-        permissions: user.permissions
-    }
-    return authHelper.createToken(payload);
+      _id: user._id,
+      email: user.email,
+      mobile: user.mobile,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      status: user.status,
+      verified: user.verified,
+      role: {
+        _id: user.role._id,
+        name: user.role.name,
+      },
+      permissions: user.permissions,
+    };
 
-}
+    return authHelper.createToken(payload);
+  }
+
+  const payload = {
+    _id: user._id,
+    email: user.email,
+    mobile: user.mobile,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    status: user.status,
+    verified: user.verified,
+    role: {
+      _id: user.role._id,
+      name: user.role.name,
+    },
+    permissions: user.permissions,
+  };
+
+  return authHelper.createToken(payload);
+};
 
 const sendOtpService = async (email) => {
+  const isUser = await userService.findUserByProperty("email", email);
 
-    const isUser = await userService.findUserByProperty('email', email);
+  if (!isUser) throw error("Account not found", 400);
+  const isOtp = await otpService.findOptByProperty({ email });
+  const otp = Math.floor(100000 + Math.random() * 900000);
 
-    if (!isUser) throw error('Account not found', 400);
-    const isOtp = await otpService.findOptByProperty({email});
-    const otp = Math.floor(100000 + Math.random() * 900000);
+  if (isOtp) {
+    await otpService.updateOtp({ email, otp, status: 0 });
+  } else {
+    await otpService.createOtp({ email, otp });
+  }
+  // Email Send
+  const send = await sendOTP(
+    email,
+    "Your Verification Code is= " + otp,
+    `${process.env.APP_NAME} email verification`
+  );
 
-    if (isOtp) {
-        await otpService.updateOtp({email, otp, status: 0})
-    } else {
-         await otpService.createOtp({email, otp});
-    }
-    // Email Send
-    const send = await sendOTP(email, "Your Verification Code is= " + otp, `${process.env.APP_NAME} email verification`);
-
-    if (send[0].statusCode !== 202) {
-        throw error('Server error occurred', 5000)
-    }
-    return otp?.otp;
-}
+  if (send[0].statusCode !== 202) {
+    throw error("Server error occurred", 5000);
+  }
+  return otp?.otp;
+};
 
 const verifyOtpService = async (email, otp, options) => {
+  const isOtp = await otpService.findOptByProperty(
+    { email, otp, status: 0 },
+    null,
+    options
+  );
 
-    const isOtp = await otpService.findOptByProperty({email, otp, status: 0}, null, options);
+  if (!isOtp) throw error("Invalid OTP", 400);
 
-    if (!isOtp) throw error('Invalid OTP', 400);
+  isOtp.status = 1;
+  if (!isOtp) throw error("Invalid OTP", 400);
 
-    isOtp.status = 1;
-    if (!isOtp) throw error('Invalid OTP', 400);
+  isOtp.status = 1;
 
-    isOtp.status = 1;
+  await isOtp.save(options);
 
-    await isOtp.save(options);
+  return userService.userUpdateService({ email }, { verified: true }, options);
+};
+const passwordChangeService = async ({
+  email,
+  oldPassword,
+  password,
+  confirmPassword,
+}) => {
+  const user = await userService.findUserByProperty("email", email);
 
-    return userService.userUpdateService({email}, {verified: true}, options );
+  const userHashPassword = user ? user.password : "";
 
-}
-const passwordChangeService = async ({email, oldPassword, password, confirmPassword})=>{
+  const isMatch = authHelper.comparePassword({
+    password: oldPassword,
+    hash: userHashPassword,
+  });
 
-    const user = await userService.findUserByProperty('email', email);
+  if (!isMatch) throw error("Old password doesn't match", 400);
 
-    const userHashPassword = user ? user.password : '';
+  const hash = authHelper.hashPassword(password);
 
-    const isMatch = authHelper.comparePassword({password: oldPassword, hash: userHashPassword});
+  return userService.passwordUpdateService({ email, hash });
+};
 
-    if (!isMatch) throw error("Old password doesn't match", 400);
+const resetPasswordService = async ({ email, otp, password, options }) => {
+  const isOtp = await otpService.findOptByProperty({ email, otp, status: 1 });
 
-    const hash = authHelper.hashPassword(password);
+  if (!isOtp) throw error("Invalid request", 400);
 
-    return userService.passwordUpdateService({email, hash});
-}
+  const isUser = await userService.findUserByProperty("email", email);
 
+  if (!isUser) throw error("Invalid request", 400);
 
-const resetPasswordService = async ({email, otp, password, options}) => {
+  const hash = authHelper.hashPassword(password);
 
-    const isOtp = await otpService.findOptByProperty({email, otp, status: 1})
+  await userService.passwordUpdateService({ email, hash, options });
 
-    if (!isOtp) throw error('Invalid request', 400);
-
-    const isUser = await userService.findUserByProperty('email', email);
-
-    if (!isUser) throw error('Invalid request', 400);
-
-    const hash = authHelper.hashPassword(password);
-
-    await userService.passwordUpdateService({email, hash, options});
-
-    return otpService.updateOtp({email, otp, status: 1, options});
-}
-
+  return otpService.updateOtp({ email, otp, status: 1, options });
+};
 
 module.exports = {
-    registerService, loginService, sendOtpService, verifyOtpService, passwordChangeService, resetPasswordService
-}
-
-
-
+  registerService,
+  loginService,
+  socialLoginService,
+  sendOtpService,
+  verifyOtpService,
+  passwordChangeService,
+  resetPasswordService,
+};
